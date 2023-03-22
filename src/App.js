dc656f43a128f072153851493c7af57cb98793b9
kindDev0813 <benjamintelford0813@gmail.com>
2023-03-22 12:23:33 -0400

    CREATE: Menupages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import dashboard from "./dashboard";
+import MenuPages from "./Pages/Menu";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+        <dashboard></dashboard>
+      </header> */}
+      <MenuPages />
+      {/* <dashboard /> */}
     </div>
   );
 }
